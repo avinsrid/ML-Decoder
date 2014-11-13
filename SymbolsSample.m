82c778aad4024a903ea707a15ce390a90254aeed
Avinash <avinash.sridhar1989@gmail.com>
2014-11-12 19:33:50 -0500

    Included ; to format the matlab code

--- SymbolsSample.m
+++ SymbolsSample.m
@@ -3,8 +3,8 @@
 clear all; clf
 M=4; % M-QAM Modulation
 N = 10 ; % Number of symbols to transmit, for test we use N = 10
-Xin = randint(1,N,M) % Our input set
-y=modulate(modem.qammod(M),Xin) % Input set is M-QAM modulated
+Xin = randint(1,N,M); % Our input set
+y=modulate(modem.qammod(M),Xin); % Input set is M-QAM modulated
 
 % Generating channel gain values between t1r1, t1r2, t2r1, t2r2
 h1 = 1/sqrt(2)*(randn(1,1) + 1i*randn(1,1));
@@ -45,9 +45,9 @@ R2 = G*ala1 + G_bar*ala2+[N11 N12];
 %[ r21 r22 ]
 % We need the matrix R in format R = [r11 + r12; r21 + r22]
 R = [R1(1,1) + R1(1,2); R2(1,1) + R2(1,2)]
-P = transpose([H ;G])
+P = transpose([H ;G]);
 Q = transpose([H_bar ;G_bar]);
-S = transpose([x3 x4])
+S = transpose([x3 x4]);
 
 % Test elements for one iteration of conditional optimization logic
 s1 = [1+i; -1+i];
@@ -57,11 +57,11 @@ s2 = [1-i; -1+i];
 %adjoint(P) did not work, hence manually calculating it.
 % ref: http://math.tutorvista.com/algebra/cofactor-matrix.html
 norm_f = (norm(H,'fro'))^2+(norm(G,'fro'))^2;
-P_ad = transpose([P(2,2) -P(2,1); -P(1,2) P(1,1)])
-C1 = (2*(P_ad)/(norm_f)) * (R-(Q*s1))
-C2 = (2*(P_ad)/(norm_f)) * (R-(Q*s2))
+P_ad = transpose([P(2,2) -P(2,1); -P(1,2) P(1,1)]);
+C1 = (2*(P_ad)/(norm_f)) * (R-(Q*s1));
+C2 = (2*(P_ad)/(norm_f)) * (R-(Q*s2));
 
 %Square Metric for symbols 1 and 2. Which ever is lower is the more
 %accurate result.
-Ms_1 = (norm(R - P*C1 - Q*s1))^2
-Ms_2 = (norm(R - P*C2 - Q*s2))^2
+Ms_1 = (norm(R - P*C1 - Q*s1))^2;
+Ms_2 = (norm(R - P*C2 - Q*s2))^2;
